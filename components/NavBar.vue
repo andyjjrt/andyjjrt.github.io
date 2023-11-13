018b8527b678776a5addbe261131898280cb1a23
andyjjrt <andyjjrt@gmail.com>
2023-11-13 15:22:10 +0800

    fix: Fix navbar display

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,16 +1,17 @@
 <template>
   <div
+    v-if="ready"
     class="navbar sticky top-0 z-10"
     :class="
       display
-        ? 'bg-base-100 backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-md transition-opacity'
-        : 'bg-transparent'
+        ? 'bg-transparent'
+        : 'bg-base-100 backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-md transition-opacity'
     "
   >
     <div class="flex-1">
-      <a class="btn btn-ghost normal-case text-xl" :class="{ hidden: !display }"
-        >andyjjrt</a
-      >
+      <a class="btn btn-ghost normal-case text-xl" :class="{ hidden: display }">
+        andyjjrt
+      </a>
     </div>
     <div class="flex-none">
       <ThemeSwitcher />
@@ -23,9 +24,10 @@ import { useWindowScroll } from "@vueuse/core";
 
 const { y } = useWindowScroll();
 
-const display = computed(() => y.value > 300);
+const display = computed(() => y.value < 300);
+const ready = ref(false);
 
-watch(y, (val) => {
-  console.log(val);
+onMounted(() => {
+  ready.value = true;
 });
 </script>
