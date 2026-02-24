985441dfdbda6a979ca0c8ad8cb87006031ca746
andyjjrt <andyjjrt@gmail.com>
2026-02-24 17:20:22 +0800

    feat: implement Nuxt UI components and update configuration

---
+++ app/composables/useLinks.ts
@@ -0,0 +1,9 @@
+export const useLinks = () => {
+  const links: Record<string, string> = {
+    github: "https://github.com/andyjjrt",
+    facebook: "https://www.facebook.com/andy.hung.7505/",
+    linkedin: "https://www.linkedin.com/in/andy-hung-245bb8184/"
+  };
+
+  return { links };
+};
