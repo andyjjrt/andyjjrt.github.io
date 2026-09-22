d7857461b4a8de5617504af60512eb693c500461
andyjjrt <andyjjrt@gmail.com>
2026-09-22 12:09:57 +0800

    Refactor code structure for improved readability and maintainability

--- app/components/Profile/Experience.vue
+++ app/components/Profile/Experience.vue
@@ -2,6 +2,7 @@
   <UPageCard title="Profile" variant="soft">
     <template #description>
       <ul class="list-disc ml-3">
+        <li>VDAR-Router: Adaptive LLMs Routing via Verbalized Query Difficulty Analysis Retrieval (<ULink to="https://arxiv.org/abs/2607.18098">EMNLP2026 Findings</ULink>)</li>
         <li>國立陽明交通大學資訊工程研究所 (NYCUCS)</li>
         <li>國立政治大學資訊科學系 (NCCUCS)</li>
         <li>GDSC NCCU 22' Core Team</li>
